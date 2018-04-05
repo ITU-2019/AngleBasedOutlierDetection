cd326741dd04baf19e0707f02235fac3a21a8b1b
Sebastian <sebab@itu.dk>
2018-04-05 14:37:11 +0200

    finishing touches

--- build/e-value-Calculator.py
+++ build/e-value-Calculator.py
@@ -9,11 +9,13 @@ def printKValues(e,n):
   k1 = 4/((((e**2)/2)-((e**3)/3)))* log(n) # Simple proofOur paper. 
   k2 = 9/((((e**2))-(2*(e**3)/3)))* log(n) #
   k3 = (e**-2)*log(n)
+  k4 = log(n) / (e**2 * log(1 / e))
   print(" \t e : " + str(e) + " \t n :" + str(n) )
   print(" \t k1 : " + str(k1))
   print(" \t k2 : " + str(k2))
   print(" \t k3 : " + str(k3))
-  return floor(k3) + 1
+  print(" \t k4 : " + str(k4))
+  return floor(k4) + 1
 
 def printE(elements,dims,reduceddims, q, e):
   maxE = 0
@@ -24,7 +26,12 @@ def printE(elements,dims,reduceddims, q, e):
   afterDists = []
   maxAfterDists = []
   minAfterDists = []
-  a = np.random.rand(elements,dims)
+
+  mu = 584
+  sigma = 14.2
+
+
+  a =  np.random.normal(mu,sigma,(elements,dims))
   res = fjlt(a.transpose(),reduceddims, q).transpose()
   #res = fjlt_usp(a.transpose(),reduceddims).transpose()
   for i in range(0, elements -1 ):
@@ -42,21 +49,22 @@ def printE(elements,dims,reduceddims, q, e):
       minE = val
     if(val > maxE):
       maxE = val
-  minE = minE - sumE/len(a)
-  maxE = maxE - sumE/len(a)
-  vals = list(map(lambda x: x -sumE/len(a), vals))
-  vals = sorted(vals)
-  #plt.hist(vals,"auto")
 
+  afterDists =    [x for _,x in sorted(zip(beforeDists,afterDists))]
+  maxAfterDists = [x for _,x in sorted(zip(beforeDists,maxAfterDists))]
+  minAfterDists = [x for _,x in sorted(zip(beforeDists,minAfterDists))]
+  beforeDists = sorted(beforeDists)
   
-  plt.hist(beforeDists,"auto", alpha=0.25, label="before")
-  plt.hist(afterDists,"auto", alpha=0.25, label= "after")    
-  plt.hist(maxAfterDists,"auto", alpha=0.25, label= "maxafter") 
-  plt.hist(minAfterDists,"auto", alpha=0.25, label= "minafter") 
+  plt.plot(afterDists,"bo", alpha=0.25, markerSize=2, label= "afterPoints")    
+  plt.plot(afterDists, alpha=0.25, label= "afterLines")
+  plt.plot(maxAfterDists, alpha=0.7, label= "maxafter") 
+  plt.plot(minAfterDists, alpha=0.7, label= "minafter") 
+  plt.plot(beforeDists, alpha=0.7, label="before")
   plt.legend(loc='upper right')  
   plt.show()
 
-e = 0.1
-k = printKValues(e,1000)
-printE(1000,1000,k, 0.9, e)
+individuals = 10000
+e = 0.7
+k = printKValues(e,individuals)
+printE(individuals,10,k, 0.9, e)
 
