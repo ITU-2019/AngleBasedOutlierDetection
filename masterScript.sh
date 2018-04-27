35bfc7a2be9b2ee5d56fb84902f7d014e578ec38
Sebastian B <seb@bgaard.dk>
2018-04-27 23:15:02 +0200

    masterscript update (#4)
    * masterscript update
    
    * add entries argument
    
    * added example comment
    
    * nicify
    
    * add execute script
    
    * Update fullExecute.sh

--- masterScript.sh
+++ masterScript.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
+# Outlier-kNN.exe "data/optdigits_39_0.txt" 1144  64 &
+# Outlier-kNN.exe "data/mfeat_69_0.txt"      410 649 &
+# Outlier-kNN.exe "data/isolet_CDE_Y.txt"    910 617 
 
 cd "results"
-# Make 10 runs.
-for i in {1..10..1}
+
+mkdir "run$1"
+cd "run$1"
+
+mkdir "$2"
+cd "$2"
+for j in 2 4 8 16 32 64 128 256 512 1024
 do
-    mkdir "run$i"
-    cd "run$i"
-
-    mkdir "optdigit"
-    mkdir "mfeat"
-    mkdir "isolet"
-
-    # Make 10,20,30...100 dimensions.
-    for j in {10..100..10}
-    do
-
-        # Test on the different data files.
-        cd "optdigit"
-
-        # idea for generating test files
-        #        file,  input file,                   dims, output file
-        # python gen.py ../../data/optdigits_39_0.txt $j    ../../data/optdigits_39_0--$j.txt
-
-        ../../Outlier--KDD08.exe "../../data/optdigits_39_0--$j.txt" 1144 $j
-        ../../Outlier--KDD12.exe "../../data/optdigits_39_0--$j.txt" 1144 $j
-        ../../Outlier--KDDABOD.exe "../../data/optdigits_39_0--$j.txt" 1144 $j
-        cd ".."
-
-        cd "mfeat"
-        ../../Outlier--KDD08.exe "../../data/mfeat_69_0--$j.txt" 410 $j
-        ../../Outlier--KDD12.exe "../../data/mfeat_69_0--$j.txt" 410 $j
-        ../../Outlier--ABOD.exe "../../data/mfeat_69_0--$j.txt" 410 $j
-        cd ".."
-
-        cd "isolet"
-        ../../Outlier--KDD08.exe "../../data/isolet_CDE_Y--$j.txt" 910 $j
-        ../../Outlier--KDD12.exe "../../data/isolet_CDE_Y--$j.txt" 910 $j
-        ../../Outlier--ABOD.exe "../../data/isolet_CDE_Y--$j.txt" 910 $j
-        cd ".."
-    done
-    cd ".."
+    ../../Outlier-ABOD.exe       "../../data_reduced/$2-$1-$j.txt" $3 $j
+    ../../Outlier-AproxABOD.exe  "../../data_reduced/$2-$1-$j.txt" $3 $j
+    ../../Outlier-LBABOD.exe     "../../data_reduced/$2-$1-$j.txt" $3 $j
+    ../../Outlier-LOF.exe        "../../data_reduced/$2-$1-$j.txt" $3 $j
+    ../../Outlier-KNN.exe        "../../data_reduced/$2-$1-$j.txt" $3 $j
+    ../../Outlier-AMS.exe        "../../data_reduced/$2-$1-$j.txt" $3 $j 
 done
+cd ".."
+
