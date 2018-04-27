72be471a4c0afde977869f506e180cef3c6d7506
MGApcDev <mgapcdev@gmail.com>
2018-04-27 23:44:53 +0200

    Fix path

--- masterScript.sh
+++ masterScript.sh
@@ -12,11 +12,11 @@ mkdir "$2"
 cd "$2"
 for j in 2 4 8 16 32 64 128 256 512 1024
 do
-    ../../Outlier-ABOD.exe       "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
-    ../../Outlier-AproxABOD.exe  "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
-    ../../Outlier-LBABOD.exe     "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
-    ../../Outlier-LOF.exe        "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
-    ../../Outlier-KNN.exe        "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
-    ../../Outlier-AMS.exe        "../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-ABOD.exe       "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-AproxABOD.exe  "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-LBABOD.exe     "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-LOF.exe        "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-KNN.exe        "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
+    ../../Outlier-AMS.exe        "../../../data_reduced/$2/$2-$1-$j.txt" $3 $j
 done
 cd ".."
